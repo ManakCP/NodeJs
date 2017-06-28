5173bf193a0c209f47a6126537e8a02a3b5c797e
ManakCP <manakchand@outlook.com>
2017-06-28 10:44:14 +0530

    port changed to dynamic

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require('express'),
     app = express(),
-    port = process.env.port || 3000,
+    port = process.env.port,
     
     bodyparser = require('body-parser'),
     path = require('path'),
